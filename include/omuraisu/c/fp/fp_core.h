877ad0974538b5bf256f585fb43c66478f2f3934
jaga <tomoki201130@gmail.com>
2026-07-16 18:59:37 +0900

    refactor: align FP naming with DJI conventions

--- include/omuraisu/c/fp/fp_core.h
+++ include/omuraisu/c/fp/fp_core.h
@@ -7,35 +7,36 @@
 extern "C" {
 #endif
 
-#define FP_MOTOR_MAX 4U
+#define FP_MOTOR_NUM 4U
 
 typedef struct {
-  int32_t enc;
+  int32_t encoder;
   uint32_t adc;
 } FpData;
 
 typedef struct {
-  uint32_t send_id_;
-  int16_t output_[FP_MOTOR_MAX];
-  FpData data_[FP_MOTOR_MAX];
-} Fp;
+  uint32_t tx_id_;
+  int16_t output_[FP_MOTOR_NUM];
+  FpData data_[FP_MOTOR_NUM];
+} FpCore;
 
 FpData om_fp_data_init();
 void om_fp_data_parse(FpData* data, const uint8_t raw[8]);
 
-Fp om_fp_core_init(uint32_t send_id);
+FpCore om_fp_core_init(uint32_t tx_id);
 
-int om_fp_core_parse(Fp* fp, uint32_t id, const uint8_t data[8], uint8_t len);
+int om_fp_core_parse(FpCore* core, uint32_t id, const uint8_t data[8],
+                     uint8_t len);
 
-void om_fp_core_set_pwm(Fp* fp, int16_t pwm, int id);
-void om_fp_core_get_output(const Fp* fp, uint8_t out[8]);
+void om_fp_core_set_pwm(FpCore* core, int16_t pwm, int id);
+void om_fp_core_get_output(const FpCore* core, uint8_t out[8]);
 
-int32_t om_fp_core_get_enc(const Fp* fp, int id);
-uint32_t om_fp_core_get_adc(const Fp* fp, int id);
-FpData om_fp_core_get_data(const Fp* fp, int id);
+int32_t om_fp_core_get_encoder(const FpCore* core, int id);
+uint32_t om_fp_core_get_adc(const FpCore* core, int id);
+FpData om_fp_core_get_data(const FpCore* core, int id);
 
 #ifdef __cplusplus
 }  // extern "C"
 #endif
 
-#endif  // FP_CORE_H
+#endif  // FP_H
--- src/fp/fp_core.c
+++ src/fp/fp_core.c
@@ -8,63 +8,64 @@ FpData om_fp_data_init() {
 }
 
 void om_fp_data_parse(FpData* data, const uint8_t raw[8]) {
-  memcpy(&data->enc, &raw[0], sizeof(data->enc));
+  memcpy(&data->encoder, &raw[0], sizeof(data->encoder));
   memcpy(&data->adc, &raw[4], sizeof(data->adc));
 }
 
-Fp om_fp_core_init(uint32_t send_id) {
-  Fp fp;
-  for (int i = 0; i < FP_MOTOR_MAX; ++i) {
-    fp.data_[i] = om_fp_data_init();
+FpCore om_fp_core_init(uint32_t tx_id) {
+  FpCore core;
+  for (int i = 0; i < FP_MOTOR_NUM; ++i) {
+    core.data_[i] = om_fp_data_init();
   }
-  fp.send_id_ = send_id;
-  memset(fp.output_, 0, sizeof(fp.output_));
-  return fp;
+  core.tx_id_ = tx_id;
+  memset(core.output_, 0, sizeof(core.output_));
+  return core;
 }
 
-int om_fp_core_parse(Fp* fp, uint32_t id, const uint8_t data[8], uint8_t len) {
-  if (len != sizeof(fp->data_[0])) {
+int om_fp_core_parse(FpCore* core, uint32_t id, const uint8_t data[8],
+                     uint8_t len) {
+  if (len != sizeof(core->data_[0])) {
     return -1;
   }
 
-  if (id <= fp->send_id_ || id > fp->send_id_ + FP_MOTOR_MAX) {
+  if (id <= core->tx_id_ || id > core->tx_id_ + FP_MOTOR_NUM) {
     return -1;
   }
 
-  const uint32_t index = id - fp->send_id_ - 1U;
-  om_fp_data_parse(&fp->data_[index], data);
+  const uint32_t index = id - core->tx_id_ - 1U;
+  om_fp_data_parse(&core->data_[index], data);
   return (int)index;
 }
 
-void om_fp_core_set_pwm(Fp* fp, int16_t pwm, int id) {
-  if (id < 1 || id > FP_MOTOR_MAX) {
+void om_fp_core_set_pwm(FpCore* core, int16_t pwm, int id) {
+  if (id < 1 || id > FP_MOTOR_NUM) {
     return;
   }
 
-  fp->output_[id - 1] = pwm;
+  core->output_[id - 1] = pwm;
 }
 
-void om_fp_core_get_output(const Fp* fp, uint8_t out[8]) {
-  memcpy(out, fp->output_, sizeof(fp->output_));
+void om_fp_core_get_output(const FpCore* core, uint8_t out[8]) {
+  memcpy(out, core->output_, sizeof(core->output_));
 }
 
-int32_t om_fp_core_get_enc(const Fp* fp, int id) {
-  if (id < 1 || id > FP_MOTOR_MAX) {
+int32_t om_fp_core_get_encoder(const FpCore* core, int id) {
+  if (id < 1 || id > FP_MOTOR_NUM) {
     return 0;
   }
-  return fp->data_[id - 1].enc;
+  return core->data_[id - 1].encoder;
 }
 
-uint32_t om_fp_core_get_adc(const Fp* fp, int id) {
-  if (id < 1 || id > FP_MOTOR_MAX) {
+uint32_t om_fp_core_get_adc(const FpCore* core, int id) {
+  if (id < 1 || id > FP_MOTOR_NUM) {
     return 0;
   }
-  return fp->data_[id - 1].adc;
+  return core->data_[id - 1].adc;
 }
 
-FpData om_fp_core_get_data(const Fp* fp, int id) {
-  if (id < 1 || id > FP_MOTOR_MAX) {
+FpData om_fp_core_get_data(const FpCore* core, int id) {
+  if (id < 1 || id > FP_MOTOR_NUM) {
     return om_fp_data_init();
   }
-  return fp->data_[id - 1];
+  return core->data_[id - 1];
 }
